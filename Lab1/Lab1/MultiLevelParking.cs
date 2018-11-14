cdb42072d8492553a4aa0b0720cf664a3b981da9
OkCur64 OkCur64 <ok.curilo2010@yandex.ru>
2018-11-15 00:49:39 +0400

    lab6

--- Lab1/Lab1/MultiLevelParking.cs
+++ Lab1/Lab1/MultiLevelParking.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,13 @@ namespace Lab1
         List<Parking<IAircraft>> parkingStages;
 
         private const int countPlaces = 16;
-
+        private int pictureWidth;
+        private int pictureHeight;        
         public MultiLevelParking(int countStages, int pictureWidth, int pictureHeight)
         {
             parkingStages = new List<Parking<IAircraft>>();
+            this.pictureWidth = pictureWidth;
+            this.pictureHeight = pictureHeight;
             for (int i = 0; i < countStages; ++i)
             {
                 parkingStages.Add(new Parking<IAircraft>(countPlaces, pictureWidth, pictureHeight));
@@ -33,5 +37,126 @@ namespace Lab1
                 return null;
             }
         }
+
+        public bool SaveData(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
+            {
+                using (BufferedStream bs = new BufferedStream(fs))
+                {
+                    //Записываем количество уровней
+                    WriteToFile("CountLeveles:" + parkingStages.Count +
+                   Environment.NewLine, fs);
+                    foreach (var level in parkingStages)
+                    {
+                        //Начинаем уровень
+                        WriteToFile("Level" + Environment.NewLine, fs);
+                        for (int i = 0; i < countPlaces; i++)
+                        {
+                            var fighter = level[i];
+                            if (fighter != null)
+                            {
+                                //если место не пустое
+                                //Записываем тип мшаины
+                                if (fighter.GetType().Name == "Plane")
+                                {
+                                    WriteToFile(i + ":Plane:", fs);
+                                }
+                                if (fighter.GetType().Name == "Fighter")
+                                {
+                                    WriteToFile(i + ":Fighter:", fs);
+                                }
+                                //Записываемые параметры
+                                WriteToFile(fighter + Environment.NewLine, fs);
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// Метод записи информации в файл
+        /// </summary>
+        /// <param name="text">Строка, которую следует записать</param>
+        /// <param name="stream">Поток для записи</param>
+        private void WriteToFile(string text, FileStream stream)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(text);
+            stream.Write(info, 0, info.Length);
+        }
+        /// <summary>
+        /// Загрузка нформации по автомобилям на парковках из файла
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public bool LoadData(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                return false;
+            }
+            string bufferTextFromFile = "";
+            using (FileStream fs = new FileStream(filename, FileMode.Open))
+            {
+                using (BufferedStream bs = new BufferedStream(fs))
+                {
+                    byte[] b = new byte[fs.Length];
+                    UTF8Encoding temp = new UTF8Encoding(true);
+                    while (bs.Read(b, 0, b.Length) > 0)
+                    {
+                        bufferTextFromFile += temp.GetString(b);
+                    }
+                }
+            }
+            bufferTextFromFile = bufferTextFromFile.Replace("\r", "");
+            var strs = bufferTextFromFile.Split('\n');
+            if (strs[0].Contains("CountLeveles"))
+            {
+                //считываем количество уровней
+                int count = Convert.ToInt32(strs[0].Split(':')[1]);
+                if (parkingStages != null)
+                {
+                    parkingStages.Clear();
+                }
+                parkingStages = new List<Parking<IAircraft>>(count);
+            }
+            else
+            {
+                //если нет такой записи, то это не те данные
+                return false;
+            }
+            int counter = -1;
+            IAircraft fighter = null;
+            for (int i = 1; i < strs.Length; ++i)
+            {
+                //идем по считанным записям
+                if (strs[i] == "Level")
+                {
+                    //начинаем новый уровень
+                    counter++;
+                    parkingStages.Add(new Parking<IAircraft>(countPlaces, pictureWidth, pictureHeight));
+                    continue;
+                }
+                if (string.IsNullOrEmpty(strs[i]))
+                {
+                    continue;
+                }
+                if (strs[i].Split(':')[1] == "Plane")
+                {
+                    fighter = new Plane(strs[i].Split(':')[2]);
+                }
+                else if (strs[i].Split(':')[1] == "Fighter")
+                {
+                    fighter = new Fighter(strs[i].Split(':')[2]);
+                }
+                parkingStages[counter][Convert.ToInt32(strs[i].Split(':')[0])] = fighter;
+            }
+            return true;
+        }        
     }
 }
